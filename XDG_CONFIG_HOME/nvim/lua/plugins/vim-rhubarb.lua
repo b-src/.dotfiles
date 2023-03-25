f6e20ca24c84c47a2f8543bfc214b7fa40d03b69
Brett Gleason <brettmgleason@gmail.com>
2023-03-25 13:16:19 -0500

    correctly load modules with no lua config functions

--- XDG_CONFIG_HOME/nvim/lua/plugins/vim-rhubarb.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/vim-rhubarb.lua
@@ -1,5 +1,7 @@
 return {
   'tpope/vim-rhubarb',
-  lazy = "true",
-  opts = {},
+  lazy = false,
+  -- can't lazy load, no lua setup function to call. have to do config this way I think
+  config = function()
+  end,
 }
