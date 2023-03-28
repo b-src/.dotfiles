36d9ca54f5cfd8cddf68be306268b42d2dcf4009
Brett Gleason <brettmgleason@gmail.com>
2023-03-27 22:18:29 -0500

    fix repo name for LuaSnip

--- XDG_CONFIG_HOME/nvim/lua/plugins/lsp.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/lsp.lua
@@ -203,7 +203,7 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
-      "L3M0N4D3/LuaSnip",
+      "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
     config = function()
