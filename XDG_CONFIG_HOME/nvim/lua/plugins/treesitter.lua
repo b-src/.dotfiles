3f4c309b3c7d85ad6022f9e866046493cb30b735
Brett Gleason <brettmgleason@gmail.com>
2023-03-27 20:10:04 -0500

    enable treesitter indents for python

--- XDG_CONFIG_HOME/nvim/lua/plugins/treesitter.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
       ensure_installed = { 'bash', 'c', 'cpp', 'css', 'dockerfile', 'go', 'html', 'ini', 'json', 'latex', 'lua', 'make', 'markdown', 'nix', 'python', 'rust', 'toml', 'typescript', 'yaml', 'zig', 'help', 'vim' },
 
       highlight = { enable = true, additional_vim_regex_highlighting = false },
-      indent = { enable = true, disable = { 'python' } },
+      indent = { enable = true, },
       incremental_selection = {
         enable = true,
         keymaps = {
