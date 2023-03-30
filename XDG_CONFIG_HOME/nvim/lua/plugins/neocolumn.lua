e83008256a49837e767fbc637d37d7dc344d63a6
Brett Gleason <brettmgleason@gmail.com>
2023-03-29 21:48:14 -0500

    add neocolumn

---
+++ XDG_CONFIG_HOME/nvim/lua/plugins/neocolumn.lua
@@ -0,0 +1,12 @@
+return {
+  "ecthelionvi/NeoColumn.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    NeoColumn = "120",
+  },
+  keys = {
+    {
+      "<leader>cc", "<cmd>ToggleNeoColumn<cr>", desc = "Toggle [C]olor [C]olumn"
+    },
+  }
+}
