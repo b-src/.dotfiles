1e3be1eec78a88318a1644408d887da11444b322
Brett Gleason <brettmgleason@gmail.com>
2023-09-09 13:43:07 -0500

    adjust hardtime config

--- XDG_CONFIG_HOME/nvim/lua/plugins/hardtime.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/hardtime.lua
@@ -3,6 +3,8 @@ return {
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
    opts = {
      enabled = true,
+     -- bump this to account for 'jk' -> <Esc> mapping
+     max_count = 3,
    },
    event = "VeryLazy",
    keys = {
