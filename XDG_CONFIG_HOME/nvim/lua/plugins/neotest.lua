ce2c96ec9e25907f5cce13dac42a976117749732
Brett Gleason <brettmgleason@gmail.com>
2023-09-08 21:48:16 -0500

    wip add neotest config

---
+++ XDG_CONFIG_HOME/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-neotest/neotest-python",
+  },
+  config = function()
+    local neotest = require("neotest")
+
+    neotest.setup({
+      adapters = {
+        require("neotest-python")({
+          dap = { justMyCode = false },
+        }),
+        -- require("neotest-plenary"),
+        -- require("neotest-vim-test")({
+        --   ignore_file_types = { "python", "vim", "lua" },
+        -- }),
+      },
+    })
+  end,
+}
