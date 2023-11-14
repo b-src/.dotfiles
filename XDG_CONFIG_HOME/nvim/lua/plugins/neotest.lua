bd517685dc0c40a88c207fc4c9e045c99596ed15
Brett Gleason <brettmgleason@gmail.com>
2023-11-13 20:56:57 -0600

    initial neotest config

--- XDG_CONFIG_HOME/nvim/lua/plugins/neotest.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/neotest.lua
@@ -21,4 +21,41 @@ return {
       },
     })
   end,
+  keys = {
+    {
+      "<leader>rt",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "[R]un the nearest [T]est",
+    },
+    {
+      "<leader>rl",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "[R]erun the [L]ast test",
+    },
+    {
+      "<leader>rf",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "[R]un the current [F]ile",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("neotest").run.run({ strategy = "dap" })
+      end,
+      desc = "[D]ebug the nearest [T]est",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("neotest").run.run_last({ strategy = "dap" })
+      end,
+      desc = "[D]ebug the [L]ast test",
+    },
+  },
 }
